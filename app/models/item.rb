b78d68f95720915bf978a116ddff2d3e3ff5a569
albator1983 <kim13kara@gmail.com>
2021-11-30 16:24:25 +0100

    create_category

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,20 @@
 class Item < ApplicationRecord
+  has_one_attached :picture
+  
+  validates :picture,
+            presence: true
+
+  belongs_to :category
+
+  has_many :join_cart_items
+  has_many :carts, through: :join_cart_items
+
+  has_many :join_order_items
+  has_many :orders, through: :join_order_items
+
+  has_many :comments
+  has_many :users, through: :comments
+
+  has_many :rating
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,13 @@
 class Order < ApplicationRecord
+
+  after_create :order_mail
+  after_create :order_cart
+
+  belongs_to :user
+
+  has_many :join_order_items
+  has_many :items, through: :join_order_items
+
+ 
+  
 end
