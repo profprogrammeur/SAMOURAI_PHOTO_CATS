4abc097a3c779b4e9fb6c1932d02e80464af5319
Naoxys <souleyk3@gmail.com>
2021-12-01 20:29:22 +0100

    ActionMailer

--- db/migrate/20211129125741_devise_create_users.rb
+++ db/migrate/20211129125741_devise_create_users.rb
@@ -10,7 +10,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :encrypted_password, null: false, default: ""
       
       ## Custom database fields
-      # t.string :name,         null: false
+      #t.string :first_name,         null: false
+      #t.string :last_name,          null: false
 
       ## Recoverable
       t.string   :reset_password_token
