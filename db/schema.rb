72a10a4893721fedf9c22f1bd9a64af708340e3b
HugClaisse <freehugh92@gmail.com>
2021-12-01 11:08:30 +0100

    footer created first shot

--- db/schema.rb
+++ db/schema.rb
@@ -45,10 +45,8 @@ ActiveRecord::Schema.define(version: 2021_12_30_104745) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.bigint "cart_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["cart_id"], name: "index_users_on_cart_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
