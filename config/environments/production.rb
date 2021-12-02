12ebe68c690a700ccc8f0029f30b76a2303b3f1d
profprogrammeur <contact@profprogrammeur.com>
2021-12-02 19:10:07 +0100

    comment after create

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,6 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-
   config.action_mailer.default_url_options = { :host => 'https://samouraiphotocats.herokuapp.com' }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
