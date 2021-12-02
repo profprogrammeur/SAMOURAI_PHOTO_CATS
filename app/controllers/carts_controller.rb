89de04d914e44451e8fc8ff5ceb15956fea869de
HugClaisse <freehugh92@gmail.com>
2021-12-02 11:00:15 +0100

    index

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,25 +1,24 @@
-class ItemsController < ApplicationController
- before_action :authenticate_user!, except: [:index, :show]
-  # before_action :authenticate_user!
+class CartsController < ApplicationController
+  def new
+     @cart = Cart.new
+  end
 
-  def index
-  	@items = Item.all
+  def create
+    
   end
 
   def show
-  	@item = Item.find(params[:id])
   end
 
-   def update
-   
+    def update
     @cart = Cart.find_by(user_id: current_user.id)
     new_item = JoinTableCartItem.create(cart_id: @cart.id, item_id: params[:id])
-
+      puts "##############################################"
+      puts params[:id]
     respond_to do |format|
       format.html { redirect_to root_path }
       format.js { }
     end
   end
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'carts/new'
+  get 'carts/create'
+  get 'carts/show'
   root 'items#index'
   get 'static_pages/secret'
   devise_for :users
   resources :users
   resources :items
   resources :orders
+   resources :carts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
