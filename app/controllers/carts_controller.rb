3c04dc4ba2ca1d4d302efeefdec595af38e8b737
profprogrammeur <contact@profprogrammeur.com>
2021-12-02 01:15:38 +0100

    cart and models

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,6 @@
 class CartsController < ApplicationController
   def new
+     @cart = Cart.new
   end
 
   def create
@@ -11,8 +12,9 @@ class CartsController < ApplicationController
 
     def update
     @cart = Cart.find_by(user_id: current_user.id)
-    # new_item = JoinTableCartItem.create(cart_id: @cart.id, item_id: params[:id])
+    new_item = JoinTableCartItem.create(cart_id: @cart.id, item_id: params[:id])
       puts "##############################################"
+      puts params[:id]
     respond_to do |format|
       format.html { redirect_to root_path }
       format.js { }
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :user_exists?, :check_user, only: [:show]
+  
   def show
+    @user = current_user
   end
 
   def edit
+    @user = current_user
   end
 
   def update
